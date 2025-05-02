a5ea764789adbc4def26a0107c69e68c04a83cd8
Callum Laing <callum.laing91@hotmail.co.uk>
2025-05-02 17:34:45 +0100

    file clean up

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup></script>
+
+<template>
+	<header>
+		<h1>Pomodoro Clock</h1>
+	</header>
+</template>
+
+<style scoped>
+h1 {
+	font-size: 3rem;
+	text-align: center;
+	padding: 25px 0;
+}
+</style>
